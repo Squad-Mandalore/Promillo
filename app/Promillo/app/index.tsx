2a3b3abb7f81e585e041ab8780f395f349ac1f24
Mortz <123571236+DrunkWindow@users.noreply.github.com>
2025-05-15 09:45:20 +0200

    [feat] sets up nativewind

--- app/Promillo/app/index.tsx
+++ app/Promillo/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import '@/global.css';
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-col flex-1 bg-gray-500"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-4xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
